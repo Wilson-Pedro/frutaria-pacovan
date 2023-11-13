8675f4ce16b54d71a61e846fe1b27e689c69c613
Wilson-Pedro <wilsonpedroneto@gmail.com>
2023-11-13 19:34:01 -0300

    feat: JOptional em CompraFrutas

--- src/connection/ConnectionFactory.java
+++ src/connection/ConnectionFactory.java
@@ -10,7 +10,7 @@ public class ConnectionFactory {
 
 	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 	private static final String URL = "jdbc:mysql://localhost:3306/db_frutaria_pacovan";
-        private static final String USER = "root";
+	private static final String USER = "root";
 	private static final String PASS = "";
 
 	public static Connection getConnection() {
